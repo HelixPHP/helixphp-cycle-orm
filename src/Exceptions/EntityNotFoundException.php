fc9747ac98fdfdbaaef511036f0e11285a7428cb
Caio Fernandes <caio.alberto.fernandes@hotmail.com>
2025-07-01 17:24:35 -0300

    refactor: Adiciona propriedades entityClass e identifier na exceção EntityNotFoundException e implementa métodos de acesso

--- src/Exceptions/EntityNotFoundException.php
+++ src/Exceptions/EntityNotFoundException.php
@@ -4,12 +4,27 @@ namespace CAFernandes\ExpressPHP\CycleORM\Exceptions;
 
 class EntityNotFoundException extends CycleORMException
 {
+    private string $entityClass;
+    private int|string $identifier;
+
     public function __construct(
         string $entityClass,
         int|string $identifier,
         ?\Throwable $previous = null
     ) {
+        $this->entityClass = $entityClass;
+        $this->identifier = $identifier;
         $message = "Entity {$entityClass} with identifier {$identifier} not found";
         parent::__construct($message, 0, $previous);
+    }
+
+    public function getEntityClass(): string
+    {
+        return $this->entityClass;
+    }
+
+    public function getIdentifier(): int|string
+    {
+        return $this->identifier;
     }
 }
